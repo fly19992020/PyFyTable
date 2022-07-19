a4580577759ca5b49e4c5c501a82c12ab5fc6bbf
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2022-07-19 20:11:26 +0800

    write

--- luffy/Main.py
+++ luffy/Main.py
@@ -1,6 +1,21 @@
 import core
 print("Welcome to PyFyTable")
 command = core.Command()
+
+
+def ln(c):
+    c = list(c)
+    while True:
+        if c[-1].isdigit():
+            c.pop(-1)
+        else:
+            break
+    r = False
+    c = "".join(c)
+    r = c.isalpha()
+    return r
+
+
 while True:
     n = input("PFT[")
     if n == "OPEN":
@@ -8,6 +23,9 @@ while True:
         command.open(l)
     elif n == "WRITE":
         l = input(">>").split(",")
+        while not(ln(l[0])):
+            print("Please re-enter it.")
+            l = input(">>").split(",")
         command.write(l[0], l[1])
     elif n == "READ":
         l = input(">>")
