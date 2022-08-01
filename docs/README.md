412158701a46d5c17823241a2aedfb461e5b2ee3
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2022-08-01 14:59:16 +0800

    v1.0.2

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,34 @@
-#PyFyTable
+# PyFyTable
+A table software implemented in Python.
+
+---
+## Help
+First open Main.py:
+``` bash
+python3 ./luffy/Main.py
+```
+### Write
+```
+[ WRITE
+>> rc,what you want to write
+```
+### READ
+```
+[ READ
+>> rc
+```
+### Save
+```
+[ SAVE
+```
+### Exit
+```
+[ EXIT
+```
+
+---
+## About
+### Version:
+v1.0.2
+### Core Version:
+v1.0.1
--- luffy/Main.py
+++ luffy/Main.py
@@ -18,41 +18,57 @@ def ln(c):
 
 while True:
     n = input("PFT[")
-    if n == "OPEN":
+    if n == "OPEN":  #open
         p = input(">>")
         try:
             command = core.Command(p)
         except:
             print('Error:File"' + p + '"is not found.Code 10')
-    elif n == "WRITE":
+
+    elif n == "WRITE":  #write
         i = input(">>").split(",")
-        if ln(i):
-            command.write(i[0], i[1])
+        f = 1  #flag
+        try:
+            l = ln(i[0])
+        except IndexError:
+            f = 0
+        if f == 1 and l:
+            print(command.write(i[0], i[1]))
         else:
             print(
-                "Error:The coordinates you enter do not look\
-                 like a correct coordinate. Code 31"
+                "Error:The coordinates you enter do not look"
+                "like a correct coordinate. Code 31"
             )
-    elif n == "READ":
-        i = input(">>").split(",")
-        if ln(i):
-            command.read(i)
+
+    elif n == "READ":  #read
+        i = input(">>")
+        f = 1  #flag
+        try:
+            l = ln(i)
+        except IndexError:
+            f = 0
+        if f == 1 and l:
+            print(command.read(i))
         else:
             print(
-                "Error:The coordinates you enter do not look\
-                 like a correct coordinate. Code 31"
+                "Error:The coordinates you enter do not look"
+                "like a correct coordinate. Code 31"
             )
-    elif n == "EXIT":
+
+    elif n == "EXIT":  #exit
         try:
             command.close()
         except AttributeError:
             pass
         finally:
             exit()
-    elif n == "SAVE":
+
+    elif n == "SAVE":  #save
         command.save()
+
     elif n == "":
         pass
+
     else:
         print('Error:\"{input}\" is not found.Code 30'.format(input=n))
         continue
